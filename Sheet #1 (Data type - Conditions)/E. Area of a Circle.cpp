1172b7f711e5ed12e83cf9b76fe4d3536071c94d
Yeasin-Arafat <24632965+yap-yeasin@users.noreply.github.com>
2023-02-21 14:28:36 +0600

    Create E. Area of a Circle.cpp

---
+++ Sheet #1 (Data type - Conditions)/E. Area of a Circle.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+  double x;
+  cin >> x ;
+  double pi = 3.141592653;
+  cout << fixed << setprecision(9);
+  cout << pi*(x*x)<<"\n";
+
+  return 0;
+}
