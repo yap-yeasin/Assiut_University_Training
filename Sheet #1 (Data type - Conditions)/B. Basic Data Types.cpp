754cfc6da80ca2415a1e5fad54d956c3c23e2af6
Yeasin-Arafat <24632965+yap-yeasin@users.noreply.github.com>
2023-02-21 14:23:17 +0600

    Create B. Basic Data Types.cpp

---
+++ Sheet #1 (Data type - Conditions)/B. Basic Data Types.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    int intt = intt;
+    long long lng = lng;
+    char ch = ch;
+    float flt = flt;
+    double d = d;
+
+    cin >> intt >> lng >> ch >> flt >> d;
+
+ 
+    cout << intt << "\n";
+    cout<< lng << "\n";
+    cout<< ch << "\n";
+    cout<< flt << "\n";
+    cout<< d<<"\n";
+
+    return 0;
+}
