a7427c34fcb8ce57aeba2b8ec97b6db5491fb157
Yeasin-Arafat <24632965+yap-yeasin@users.noreply.github.com>
2023-02-21 14:27:59 +0600

    Create D. Difference.cpp

---
+++ Sheet #1 (Data type - Conditions)/D. Difference.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+  long long X,Y,A,B;
+  cin >> X >> Y >> A >> B;;
+
+  cout <<"Difference"<<" = "<< (X*Y)-(A*B) <<"\n";
+  return 0;
+}
