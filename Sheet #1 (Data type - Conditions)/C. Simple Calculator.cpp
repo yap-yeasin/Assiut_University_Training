1e6a34f18af3e97365df5ab7d72378361f80250d
Yeasin-Arafat <24632965+yap-yeasin@users.noreply.github.com>
2023-02-21 14:26:51 +0600

    Create C. Simple Calculator.cpp

---
+++ Sheet #1 (Data type - Conditions)/C. Simple Calculator.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  long long X,Y;
+  cin >> X >> Y;
+
+  cout <<X<<" + "<<Y<<" = "<<X+Y<<"\n";
+  cout <<X<<" * "<<Y<<" = "<<X*Y<<"\n";
+  cout <<X<<" - "<<Y<<" = "<<X-Y;
+  return 0;
+}
