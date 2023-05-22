7f2aa79b3db882e108f7281e6ad5799b01efa914
Yeasin-Arafat <24632965+yap-yeasin@users.noreply.github.com>
2023-05-22 10:20:58 +0600

    Create Q. Coordinates of a Point.cpp

---
+++ Sheet #1 (Data type - Conditions)/Q. Coordinates of a Point.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    float X,Y;
+    cin >> X >> Y;
+    if (X>0 && Y>0){
+        cout<<"Q1"<<endl;
+    }
+    else if(X<0 && Y>0){
+        cout<<"Q2"<<endl;
+    }
+    else if(X<0 && Y<0){
+        cout<<"Q3"<<endl;
+    }
+    else if(X>0 && Y<0){
+        cout<<"Q4"<<endl;
+    }
+    else if(X>0 || X<0 && Y == 0){
+        cout<<"Eixo X"<<endl;
+    }
+    else if(Y>0 || Y<0 && X == 0){
+        cout<<"Eixo Y"<<endl;
+    }
+    else if(Y==0 && X == 0){
+        cout<<"Origem"<<endl;
+    }
+    return 0;
+}
