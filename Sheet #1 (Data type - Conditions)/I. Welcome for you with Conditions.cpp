7406e020633b1c1c054e42566f1632846867da99
Yeasin-Arafat <24632965+yap-yeasin@users.noreply.github.com>
2023-05-22 10:08:28 +0600

    Create I. Welcome for you with Conditions.cpp

---
+++ Sheet #1 (Data type - Conditions)/I. Welcome for you with Conditions.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+  double x,y;
+  cin >> x >> y;
+  if (x >=y){
+    cout<<"Yes";
+  }
+  else{
+    cout<<"No";
+  }
+
+  return 0;
+}
