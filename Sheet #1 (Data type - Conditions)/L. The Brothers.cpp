b07cec57dcb5b42abcc2ae0786ce8d4a9b168fd4
Yeasin-Arafat <24632965+yap-yeasin@users.noreply.github.com>
2023-05-22 10:13:43 +0600

    Create L. The Brothers.cpp

---
+++ Sheet #1 (Data type - Conditions)/L. The Brothers.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+int main()
+{
+    // string S,N,T,T1,s1[4],n1[4];  
+    // getline(cin, S); 
+    // getline(cin, N);
+    string a,b,c,d;
+    cin >> a>>b>>c>>d;
+ 
+    // stringstream X(S);
+    // stringstream Y(N); 
+ 
+    // int i = 0;
+    // while (getline(X, T, ' ')&&getline(Y, T1,' ')) {  
+ 
+    //     s1[i]= T;
+    //     n1[i]=T1;
+    //     i+=1;
+ 
+    // }  
+    // // cout << s1[1] << endl;
+    // // cout << n1[1] << endl;
+    if (b==d){
+    // if (s1[1]==n1[1]){
+       cout <<"ARE Brothers"<<"\n"; 
+    }
+    else{
+        cout <<"NOT"<<"\n";
+    }
+ 
+    return 0;
+}
