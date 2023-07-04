f9d28626492b45d815ff5ad11b29a43a5e25acfe
Yeasin-Arafat <24632965+yap-yeasin@users.noreply.github.com>
2023-07-04 09:20:34 +0600

    Create B. Let's use Getline.cpp

---
+++ Sheet #4 (Strings)/B. Let's use Getline.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// int main(){
+//     string s,t;
+//     getline(cin,s);
+//     for(int i=0;i<s.size();i++){
+//         if(s[i] == '\\'){
+//             break;
+//         }
+//         cout<<s[i];
+//     }
+// }
+
+int main(){
+    string s,t;
+    getline(cin,s,'\\');
+    // getline(to read ,str store, delim: It is the delimitation 
+    // character which tells the function to stop 
+    // reading further input after reaching this character.)
+    cout<<s;
+
+}
