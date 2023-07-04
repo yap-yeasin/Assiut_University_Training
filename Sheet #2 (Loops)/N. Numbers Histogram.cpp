c9b851f33909e4d72e87b6669e8f0554538db264
Yeasin-Arafat <24632965+yap-yeasin@users.noreply.github.com>
2023-07-04 08:33:06 +0600

    Create N. Numbers Histogram.cpp

---
+++ Sheet #2 (Loops)/N. Numbers Histogram.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{   
+    int li,n1,n2,n3,n4,n5,a;
+    char ch;
+    cin >>ch;
+    cin >>li;
+    for(int i=1;i<=li;i++){
+        cin>>a;
+        int j=1;
+        while(j<=a){
+            cout<<ch;
+            j++;
+        }
+        cout<<endl;
+    }
+}
