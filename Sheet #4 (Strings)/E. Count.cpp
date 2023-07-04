1f3d7b9fe29027372b7a3f04bb683e94b5cd7afa
Yeasin-Arafat <24632965+yap-yeasin@users.noreply.github.com>
2023-07-04 09:25:18 +0600

    Create E. Count.cpp

---
+++ Sheet #4 (Strings)/E. Count.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ 
+    string x ;
+    cin >> x ;
+    int sum =0;
+    for(int i=0;i<x.size();i++){
+        sum+=(x[i]-'0');
+    }
+    cout<<sum;
+    return 0;
+}
