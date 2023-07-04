b3fe7e796da5888246458c0826061affe07f75b3
Yeasin-Arafat <24632965+yap-yeasin@users.noreply.github.com>
2023-07-04 09:18:54 +0600

    Create A. Create A New String.cpp

---
+++ Sheet #4 (Strings)/A. Create A New String.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    string s,t;
+    cin >> s>>t;
+    cout<<s.size()<<" "<<t.size()<<endl;
+    cout<<s<<" "<< t<<endl;
+
+ 
+return 0;
+}
