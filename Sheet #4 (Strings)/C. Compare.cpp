c2d9e28142af6f51fb7b88463943ed8a59759007
Yeasin-Arafat <24632965+yap-yeasin@users.noreply.github.com>
2023-07-04 09:23:24 +0600

    Create C. Compare.cpp

---
+++ Sheet #4 (Strings)/C. Compare.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ 
+    string x , y;
+    cin >> x >> y;
+    if (x < y)
+        cout << x;
+    else
+        cout << y;
+    
+    return 0;
+}
