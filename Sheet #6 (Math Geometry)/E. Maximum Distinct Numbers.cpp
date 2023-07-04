79f6d57edd64e13b9ea864b06f32f419df378ea4
Yeasin-Arafat <24632965+yap-yeasin@users.noreply.github.com>
2023-07-04 10:26:23 +0600

    Create E. Maximum Distinct Numbers.cpp

---
+++ Sheet #6 (Math Geometry)/E. Maximum Distinct Numbers.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// n^2+n-2(num)
+// https://www.programiz.com/cpp-programming/examples/quadratic-roots
+
+int main(){
+
+    long long int n,sum=0,r;
+    cin >>n;
+
+    cout<< ((int)((0.5)*(-1+sqrt(1+8*n)))) <<endl;  // + value 
+}
