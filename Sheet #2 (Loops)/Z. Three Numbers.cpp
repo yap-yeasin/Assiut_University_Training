bdaee341535c50f863f3a723a7804d4b839c2dfe
Yeasin-Arafat <24632965+yap-yeasin@users.noreply.github.com>
2023-07-04 08:44:25 +0600

    Create Z. Three Numbers.cpp

---
+++ Sheet #2 (Loops)/Z. Three Numbers.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{   
+    long long k,s,x,y,z;
+    cin >> k>>s;
+    int count=0;
+	for (int i = 0; i <=k ; i++){
+        for(int j=0;j<=k;j++){
+            // y = i;
+            // z = j;
+            x=s-j-i;
+            if (x>=0&&x<=k){
+                count+=1;
+            }
+        }
+    }
+    cout<<count<<endl;
+	return 0;
+}
