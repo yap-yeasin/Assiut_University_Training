5f1baf467cd3c3b21763cf3fc9bc65dcd7d95286
Yeasin-Arafat <24632965+yap-yeasin@users.noreply.github.com>
2023-08-11 23:29:32 +0600

    Update M. Capital or Small or Digit.cpp

--- Sheet #1 (Data type - Conditions)/M. Capital or Small or Digit.cpp
+++ Sheet #1 (Data type - Conditions)/M. Capital or Small or Digit.cpp
@@ -6,13 +6,13 @@ int main()
     char X;
     cin >> X;
     int a = int(X);
-    if(a>=65 && a<=90){
+    if(a>='A' && a<='Z'){
         cout<<"ALPHA"<<"\n"<<"IS CAPITAL"<< endl;
         }
-    else if (a>=97 && a<=122) {
+    else if (a>='a' && a<='z') {
         cout<<"ALPHA"<<"\n"<<"IS SMALL"<< endl;
         } 
-    else if (a>=47 && a<=57) {
+    else if (a>='0' && a<='9') {
         cout<<"IS DIGIT"<< endl;
         }
     return 0;
