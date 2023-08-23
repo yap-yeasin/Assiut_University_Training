2501189e85e75947c4834e99a4d3727321a79a1e
Yeasin-Arafat <24632965+yap-yeasin@users.noreply.github.com>
2023-08-23 16:02:53 +0600

    Create C._Next_Alphabet.cpp

---
+++ Contest #1/C._Next_Alphabet.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define ll long long 
+
+int main()
+{
+    char x;
+    cin>>x;
+    int ans = x+1;
+    if (x=='z'){
+        cout<<"a"<<endl;
+    }
+    else{
+        cout<<(char)ans<<endl;
+    }
+    return 0;
+}
